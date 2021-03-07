af9fc8df95b39cb3277ab9af9146495a52e6881a
Wanderson Castro <wandersonce1@hotmail.com>
2021-03-07 14:45:41 -0800

    Countdown Component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,8 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <title>CheerUp - Don't get tired!</title>
+                    <title>CheerUp | Don't get tired!</title>
+                    <link rel="shortcut icon" href="images/Upupsmall.svg" type="image/x-icon" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
                 </Head>

cfa42bd108d6e28f066c2358cebf8dc22ac1e9dc
Wanderson Castro <wandersonce1@hotmail.com>
2021-03-20 12:45:07 -0700

    challenges json

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+    const { startNewChallenge } = useContext(ChallengesContext);
 
     const [time, setTime] = useState(0.05 * 60);
     const [isActive, setisActive] = useState(false);
@@ -34,6 +36,7 @@ export function Countdown() {
         } else if (isActive && time === 0) {
             sethasFinished(true);
             setisActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 

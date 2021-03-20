77ef61c8b8a2929aa52cc3437f7a7c3a762cae19
Wanderson Castro <wandersonce1@hotmail.com>
2021-03-20 13:11:50 -0700

    Using Context to exp bar and level

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,21 +1,26 @@
 import { createContext, useState, ReactNode } from 'react';
 import challenges from '../../challenges.json';
 
+// defining type for JSON file
 interface Challenge {
     type: string;
     description: string;
     amount: number;
 }
 
+// defining types for each tag used 
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
     activeChallenge: Challenge;
+    experienceToNextLevel: number;
     levelUp: () => void;
     startNewChallenge: () => void;
+    resetChallenge: () => void;
 }
 
+// this will turn able to use any type for the children that is using on _app.tsx
 interface ChallengesProviderProps {
     children: ReactNode;
 }
@@ -29,15 +34,23 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [challengesCompleted, setChallengesCompleted] = useState(0);
     const [activeChallenge, setActiveChallenge] = useState(null);
 
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
     function levelUp() {
         setLevel(level + 1);
     }
 
+    // It will look for a task on json file and show on challengeBox
     function startNewChallenge() {
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
 
         setActiveChallenge(challenge);
+    }
+
+    // This will reset the box if you click failed button on the box
+    function resetChallenge() {
+        setActiveChallenge(null);
     }
 
     return (
@@ -46,9 +59,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 level,
                 currentExperience,
                 challengesCompleted,
+                experienceToNextLevel,
                 levelUp,
                 startNewChallenge,
-                activeChallenge
+                activeChallenge,
+                resetChallenge
             }}>
 
             {children}
